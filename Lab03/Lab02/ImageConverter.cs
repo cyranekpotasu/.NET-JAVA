521bb5f4a74765296d450cac1d2bfb2c4d6d3066
cyranekpotasu <cyran.norbert97@gmail.com>
2019-04-28 00:13:13 +0200

    Add ImageConverter class

---
+++ Lab03/Lab02/ImageConverter.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Windows.Media.Imaging;
+
+namespace Lab03
+{
+    static class ImageConverter
+    {
+        public static byte[] ToByteArray(BitmapImage image)
+        {
+            byte[] data;
+            var encoder = new JpegBitmapEncoder();
+            encoder.Frames.Add(BitmapFrame.Create(image));
+            using (var ms = new MemoryStream())
+            {
+                encoder.Save(ms);
+                data = ms.ToArray();
+            }
+            return data;
+        }
+
+        public static BitmapImage ToBitmapImage(byte[] bytearray)
+        {
+            using (var ms = new MemoryStream(bytearray))
+            {
+                var image = new BitmapImage();
+                image.BeginInit();
+                image.CacheOption = BitmapCacheOption.OnLoad;
+                image.StreamSource = ms;
+                image.EndInit();
+                image.Freeze();
+                return image;
+            }
+        }
+    }
+}
