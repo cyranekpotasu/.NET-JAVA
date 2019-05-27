9c27df0bee31ea8d0d0c8dd34d6cf31b5cc5e4c9
cyranekpotasu <cyran.norbert97@gmail.com>
2019-05-27 23:25:27 +0200

    Create game grid in GameController

--- snake/src/snake/GameController.java
+++ snake/src/snake/GameController.java
@@ -1,5 +1,21 @@
 package snake;
 
-public class GameController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GameController implements Initializable {
+    @FXML
+    private Canvas canvas;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Grid grid = new Grid(canvas.getWidth(), canvas.getHeight());
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        grid.paint(gc);
+    }
 }
