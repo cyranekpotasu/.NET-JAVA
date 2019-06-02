818e14b452d6e43b0c44f7111be71d86768bad22
cyranekpotasu <cyran.norbert97@gmail.com>
2019-06-02 22:54:42 +0200

    Generate food at random locations on the grid

--- snake/src/snake/Grid.java
+++ snake/src/snake/Grid.java
@@ -3,6 +3,8 @@ package snake;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
+import java.util.Random;
+
 public class Grid implements Paintable {
     private static final Color BG_COLOR = new Color(0.1, 0.1, 0.1, 1);
 
@@ -10,12 +12,14 @@ public class Grid implements Paintable {
     private final int cols;
 
     private Snake snake;
+    private Food food;
 
     Grid(final double width, final double height, Snake snake) {
         cols = (int) width / SQUARE_SIZE;
         rows = (int) height / SQUARE_SIZE;
 
         this.snake = snake;
+        food = new Food(getRandomPoint());
     }
 
     public void update() {
@@ -27,9 +31,15 @@ public class Grid implements Paintable {
         gc.setFill(BG_COLOR);
         gc.fillRect(0, 0, rows * SQUARE_SIZE, cols * SQUARE_SIZE);
         snake.paint(gc);
+        food.paint(gc);
     }
 
     public Snake getSnake() {
         return snake;
+    }
+
+    private Vec2D getRandomPoint() {
+        Random random = new Random();
+        return new Vec2D(random.nextInt(rows), random.nextInt(cols));
     }
 }
